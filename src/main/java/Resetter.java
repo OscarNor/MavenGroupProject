5bd96aea1f30f75d4b6442e6d248a145bd470a35
HampusOls1203 <hammol1203@skola.goteborg.se>
2022-02-12 12:24:15 +0100

    Updated Resetter

--- src/main/java/Resetter.java
+++ src/main/java/Resetter.java
@@ -1,8 +1,39 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Resetter {
 
     /**
     * Empties the board and allows for a new game to be played. Contains a button to manually reset the whole game.
     */
+    private Window window;
+    private JButton resetButton;
+
+    public Resetter(Window window)
+    {
+        this.window = window;
+        resetButton = window.getResetGame();
+        resetButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                hardReset();
+            }
+        });
+    }
+
+    private void hardReset()
+    {
+        window.resetBoard();
+        window.resetPlayers();
+    }
+
+    public void reset()
+    {
+        window.resetBoard();
+    }
 
     // 4
 }
