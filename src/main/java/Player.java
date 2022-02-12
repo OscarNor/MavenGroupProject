bf4e95f9f69ff28ba5a4ba4aff9e68b4b8e73105
HampusOls1203 <hammol1203@skola.goteborg.se>
2022-02-12 11:30:28 +0100

    Adding Getters

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -4,8 +4,20 @@ public class Player {
     * Used for holding the score. Two ints that describe score and player number. Player 1 starts.
      */
 
-    // 5
-    public Player(){
+    private int playerNumber;
 
+    private int playerScore;
+
+    public Player(int player, Window window)
+    {
+        this.playerNumber = player;
+        this.playerScore = 0;
     }
+
+    public void setPlayerScore(int playerScore)
+    {
+        this.playerScore = playerScore;
+    }
+
+    // 5
 }
