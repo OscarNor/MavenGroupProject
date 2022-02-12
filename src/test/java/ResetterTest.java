84c2f9baa5f631c1d515d258c4c63f5a580b1e8d
HampusOls1203 <hammol1203@skola.goteborg.se>
2022-02-12 16:06:16 +0100

    Player och Resetter test

--- src/test/java/ResetterTest.java
+++ src/test/java/ResetterTest.java
@@ -1,2 +1,25 @@
-public class ResetterTest {
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ResetterTest
+{
+    Window window = new Window();
+    Resetter resetter = new Resetter(window);
+    Player player = new Player(1, window);
+    JButton resetButton = new JButton();
+
+    @Test
+    public void getHardReset()
+    {
+        player.setPlayerScore(3);
+        assertEquals(0, player.getPlayerScore());
+    }
+
+    public void getReset()
+    {
+        
+    }
 }
